860af7d31eb7695c6bbaab55ba01144a69b56e87
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-04-23 15:00:15 +0300

    done lab6

--- src/main/java/corejava/chapter3/lab6/DigitSequence.java
+++ src/main/java/corejava/chapter3/lab6/DigitSequence.java
@@ -1,3 +1,4 @@
+/*
 package corejava.chapter3.lab6;
 
 import java.util.Iterator;
@@ -5,20 +6,25 @@ import java.util.Iterator;
 public class DigitSequence implements Iterator<Integer> {
 
     private int number;
+    int count = 0;
+    int size = 10;
 
-    DigitSequence(int number){this.number=number;}
+    DigitSequence(int number) {
+        this.number = number;
+    }
 
     @Override
     public boolean hasNext() {
-        return false;
+        return size != count++;
     }
 
     @Override
     public Integer next() {
-        return null;
+        return number;
     }
 
     @Override
     public void remove() {
     }
 }
+*/
--- src/main/java/corejava/chapter3/lab6/Lab6.java
+++ src/main/java/corejava/chapter3/lab6/Lab6.java
@@ -3,8 +3,10 @@ package corejava.chapter3.lab6;
 public class Lab6 {
     public static void main(String[] args) {
         DigitSequence digitSequence = new DigitSequence(1);
-
         digitSequence.remove();
+        while (digitSequence.hasNext()){
+            System.out.println(digitSequence.next());
+        }
         digitSequence.forEachRemaining(Object::toString);
     }
 }
